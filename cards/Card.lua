a46f29e6ad6f2bb9f563e9cf7c85d001d5c221e6
Stephane-N <s.nguyen@hotmail.fr>
2019-07-21 20:52:36 +0200

    Add few files

---
+++ cards/Card.lua
@@ -0,0 +1,9 @@
+local Card = {}
+
+local cards = require('cards.cardConstants')
+Card.class = 'Card'
+
+function Card:new(id)
+  local card
+  return card
+end
---
+++ cards/cardConstants.lua
@@ -0,0 +1,11 @@
+local cardList = {}
+
+local hache
+hache.name = 'Hache'
+hache.attack = 6
+hache.def = -2
+
+
+cardList['hache'] = hache
+
+return cardList
