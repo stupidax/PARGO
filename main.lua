746c8f5aa1faeec44d03fb7831ea9aabc3e9123c
stupidax <stupidax@hotmail.com>
2019-07-21 16:23:49 +0200

    First Pull

---
+++ main.lua
@@ -0,0 +1,36 @@
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+love.window.setMode(960,540)
+love.window.setTitle('Pargo')
+
+
+function love.load(arg)
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+--provisoire
+--bg
+test = love.graphics.newImage('asset/basePlateau.png')
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+--scale
+love.graphics.scale(2,2)
+
+--draw bg
+love.graphics.draw(test)
+  
+end
+
+function love.keypressed(key)
+--quit
+if key == 'escape' then
+  love.event.push('quit')
+end
+  
+  
+end
